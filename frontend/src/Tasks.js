4ed80902352296dd34bf0b23763d651cd64971b0
briancoco <briannguyencoco@gmail.com>
2022-12-29 12:20:10 -0600

    refactored feed styling

--- frontend/src/Tasks.js
+++ frontend/src/Tasks.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import {BsCheckCircleFill, BsCheckCircle, BsPencilFill, BsFillTrashFill} from 'react-icons/bs'
 const Tasks = ({task}) => {
   return (
     <div className='feed-task'>
-      <h5>{task.name.length <= 25 ? task.name : task.name.substring(0, 25) + '...'}</h5>
-      <h5>{`completed: ${task.completed}`}</h5>
+      <div className='task-descrip'>
+        <h3>{task.name.length <= 25 ? task.name : task.name.substring(0, 25) + '...'}</h3>
+        <p>{task.description.length <= 25 ? task.description : task.description.substring(0, 25) + '...'}</p>
+      </div>
+      <div className='task-icons'>
+        {task.completed ? <BsCheckCircleFill /> : <BsCheckCircle />}
+        <BsPencilFill />
+        <BsFillTrashFill />
+      </div>
     </div>
   )
 }
